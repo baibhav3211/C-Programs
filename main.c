8f22417ee96b6d727fb9bf51ef6b179111703579
baibhav3211 <53977165+baibhav3211@users.noreply.github.com>
2020-10-12 22:33:10 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,159 +1,84 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node{
+struct Node {
     int data;
-    struct node* link;
+    struct Node * next;
 };
-struct node* head;
-void insert(int data){
-    struct node* temp=(struct node*)malloc(sizeof(struct node));
-    temp->data = data;
-    temp->link = NULL;
-    if(head == NULL){
-        head = temp;
-    }
-    else{
-        struct node* temp2 = head;
-        while(temp2->link!= NULL){
-            temp2 = temp2->link;
-        }
-        temp2->link = temp;
-    }
-}
-void print(){
-    if(head == NULL){
-        printf("the list is empty\n");
-        return;
-    }
-    printf("the list is : ");
-    struct node* temp = head;
-    while(temp!=NULL){
-        printf("%d ",temp->data);
-        temp = temp->link;
-    }
-    printf("\n");
-}
-void begining(int data){
-    struct node* temp=(struct node*)malloc(sizeof(struct node));
-    temp->data = data;
-    temp->link = head;
-    head = temp;
-}
-void index(int data , int n){
-    struct node* temp=(struct node*)malloc(sizeof(struct node));
-    temp->data = data;
-    if(n == 1){
-        temp->link = head;
-        head = temp;
-    }
-    else{
-        struct node* temp2 = head;
-        for(int i=0;i<n-2;i++){
-            temp2=temp2->link;
-        }
-        temp->link=temp2->link;
-        temp2->link=temp;
-    }
-}
-void Deleteatn(int n)
+
+void linkedListTraversal(struct Node *ptr,struct node *head)
 {
-    struct node* temp1 = head;
-    if(n==1){
-        head = temp1->link;
-        free(temp1);
-        return;
-    }
-    int i;
-    for(i=0;i<n-2;i++)
+    do
     {
-        temp1 = temp1->link;
-
-    }
-    struct node* temp2 = temp1->link;
-    temp1->link = temp2->link;
-    free(temp2);
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }while(ptr!=head);
 }
 
-void delatbegin(){
-    struct node* temp1 = head;
-    head = temp1->link;
-    free(temp1);
-    return;
+struct Node* Reverse(struct Node * head)
+{
+        struct Node* current = head; 
+        struct Node *prev = NULL, *next = NULL; 
+  
+        while (current != NULL) { 
+            // Store next 
+            next = current->next; 
+  
+            // Reverse current node's pointer 
+            current->next = prev; 
+  
+            // Move pointers one position ahead. 
+            prev = current; 
+            current = next; 
+        } 
+        head = prev;
+        return head;
 }
 
-void delatend(){
-    struct node* temp1 = head;
-    struct node* prev;
-    while(temp1->link!=NULL){
-        prev = temp1;
-        temp1 = temp1->link;
-    }
-    if(temp1 == head){
-        head = NULL;
-        free(temp1);
-        return;
-    }
-    prev->link = NULL;
-    free(temp1);
-}
 
-void reverse(){
-    struct node* curr= head;
-    struct node* next= NULL;
-    struct node* prev= NULL;
-    while (curr!= NULL){
-        next = curr->link;
-        curr->link = prev;
-        prev = curr;
-        curr = next;
-    }
-    head = prev;
-}
 
-int main(){
-    while(1){
-        int a,data,ind;
-        printf("1 : To enter at begining\n");
-        printf("2 : To enter at end\n");
-        printf("3 : Enter at a particular index\n");
-        printf("4 : print\n");
-        printf("5 : exit\n");
-        printf("6 : delete at n\n");
-        printf("7 : delete at begining\n");
-        printf("8 : delete at end\n");
-        printf("9 : reverse the linkedlist\n");
-        printf("Enter any choice : ");
-        scanf("%d",&a);
-        switch(a){
-            case 1: printf("Enter value :\n");
-                scanf("%d",&data);
-                begining(data);
-                break;
-            case 2: printf("Enter value :\n");
-                scanf("%d",&data);
-                insert(data);
-                break;
-            case 3: printf("Enter value and index :\n");
-                scanf("%d%d",&data,&ind);
-                index(data,ind);
-                break;
-            case 4: print();
-                break;
-            case 5: exit(0);
-            case 6: printf("Enter index :\n");
-                scanf("%d",&data);
-                Deleteatn(data);
-                break;
-            case 7: delatbegin();
-                break;
-            case 8: delatend();
-                break;
-            case 9: reverse();
-                break;
-
-            default : printf("Enter value input :\n");
+int main()
+{
+   struct Node * head=NULL;
+   struct Node * current;
+   struct Node * rev_head;
+   struct Node *temp;
+   int num=0;
+   
+    printf("Enter number of elements of linked list: \n");
+    scanf("%d",&num);
+    
+    for(int i =0; i < num; i++)
+    {
+        struct Node* current = (struct Node *)malloc(sizeof(struct Node)); 
+        scanf( "%d", &current->data);      
+       // current->next = head; 
+       if(i==0)
+        {
+            head = current;  
+            temp=current;}
+        else
+        {
+            temp->next=current;
+            temp=current;
         }
-
     }
+     temp->next = head;
+    
+    
+    
+    printf("Linked list before insertion\n");
+    linkedListTraversal(head,head);
+    // printf("Linked list After Reversal\n");
+    // rev_head = Reverse(head);
+    // linkedListTraversal(rev_head);
+    
+return 0;
+    
+    
 }
+
+
+
+
+
